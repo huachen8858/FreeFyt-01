06ad7172cc008c02e7a496d8a04985fc128b1996
huachen8858 <huachen8858@gmail.com>
2023-10-14 17:37:34 +0800

    update index sidebar and store

---
+++ product_management.php
@@ -0,0 +1,4 @@
+<?php include './index-parts/html-head.php' ?>
+<?php include './index-parts/sidebar%2BTopbar.php' ?>
+<?php include './index-parts/footer%2Bscripts.php' ?>
+<?php include './index-parts/html-foot.php' ?>
