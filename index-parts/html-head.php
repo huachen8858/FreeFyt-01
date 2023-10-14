e44df0c86abcd7fc38adc1323cb02226c487eeec
huachen8858 <huachen8858@gmail.com>
2023-10-14 20:13:34 +0800

    update title

--- index-parts/html-head.php
+++ index-parts/html-head.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>首頁 - FreeFyt</title>
+    <title><?= isset($title) ? "{$title} - FreeFyt" : 'FreeFyt' ?></title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
