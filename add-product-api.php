d8ad6834726ca1d5b638158fe77ba646879b6d5e
huachen8858 <huachen8858@gmail.com>
2023-10-18 17:21:35 +0800

    主 次分類選單

--- add-product-api.php
+++ add-product-api.php
@@ -1,36 +1,39 @@
 <?php 
 require './index-parts/connect_db.php';
 
+// 2023 10 18 5:00 這版api 無法成功上傳資料，且有抓到填寫的data 但 success 顯示false
+
+
 // 宣告變數 避免使用者點開此 api 會出現 warning，通常不要直接看api檔案
-$output =[
+$output = [
   'postData' => $_POST,
-  'sucess' => false,
+  'success' => false,
   'errors' => [],
 ];
+// echo json_encode($output);
+// exit;
 
 // 告訴用戶端格式為JSON
 header('Content-Type: application/json');
 
 // 資料寫入前要檢查: 除更多圖片其他欄位必填
-if (empty($_POST['name']) or empty($_POST['price']) or empty($_POST['category']) or empty($_POST['subCategory']) or empty($_POST['descriptions'])  or empty($_POST['mainImg']) or empty($_POST['inventory']) or empty($_POST['launch'])) {
+if (empty($_POST['name']) or empty($_POST['price']) or empty($_POST['cate1']) or empty($_POST['cate2']) or empty($_POST['descriptions']) or empty($_POST['inventory']) or empty($_POST['launch'])) {
   $output['errors']['form'] = '缺少欄位資料';
   echo json_encode($output);
   exit;
 };
 
 // 避免錯誤訊息：避免沒有值時 warning 訊息跑到 html 標籤中
-$name = $_POST['name'];
-$price = $_POST['price'];
-$category = $_POST['category'];
-$subCcategory = $_POST['subCategory'];
-$descriptions = $_POST['descriptions'];
-$mainImg = $_POST['mainImg'];
-$moreImg = $_POST['moreImg'] ?? NULL;
-$inventory = $_POST['inventory'];
-$launch = $_POST['launch'];
+$name = $_POST['name'] ?? '';
+$price = $_POST['price'] ?? '';
+$cate1 = $_POST['cate1'] ?? '';
+$cate2 = $_POST['cate2'] ?? '';
+$descriptions = $_POST['descriptions'] ?? '';
+$inventory = $_POST['inventory'] ?? '';
+$launch = $_POST['launch'] ?? '';
 
 # 後端檢查
-// $isPass = true;
+$isPass = true;
 
 //如果庫存量為0,launch設定為0
 if (intval($inventory) === 0) {
@@ -59,52 +62,29 @@ if (!empty($_POST['name'])) {
   $pid = 'FYT-' . $currentDate . '-' . $numberFormatted;
 }
 
-function getMaxProductNumber($pdo, $currentDate) {
-  // 编写SQL查询语句，查找与当前日期匹配的记录中的最大序号
-  $sql = "SELECT MAX(SUBSTRING_INDEX(product_id, '-', -1)) AS max_number 
-          FROM product 
-          WHERE product_id LIKE :date_prefix";
-
-  $stmt = $pdo->prepare($sql);
-
-  $datePrefix = 'FYT-' . $currentDate . '-%';
-  $stmt->bindParam(':date_prefix', $datePrefix, PDO::PARAM_STR);
-  $stmt->execute();
-
-  $maxNumberResult = $stmt->fetch(PDO::FETCH_ASSOC);
-
-  // 提取最大序号
-  $maxNumber = (int)$maxNumberResult['max_number'];
-
-  return $maxNumber;
-  }
-
-
-
 
 # 與資料庫串接
 // 新增功能： ?用來佔位
-$sql = "INSERT INTO `product`(
-    -- `product_id`, 
-    `name`, `category`, `subCategory`, `price`, `descriptions`, `inventory`, `purchase_qty`, `create_date`, `launch`
+$sql = "INSERT INTO `product_list`(
+    `product_id`, 
+    `name`, `price`, `category`, `descriptions`, `inventory`, `purchase_qty`, `launch`, `create_date`
   ) VALUES (
-    -- ?, 
-    ?, ?, ?, ?, ?, ?, '0', NOW(),?
+    ?, ?, ?, ?, ?, ?, 0, ?, NOW()
   )";
+
 
 // pdo 先準備：並沒有真的執行，會先拿到pdo statement 的物件
 $stmt = $pdo->prepare($sql);
 
 // pdo stmt 執行：把表單拿到的值丟到上方的 ?
 $stmt->execute([
-  // $pid,
+  $pid,
   $name,
-  $category,
-  $subCategory,
   $price,
+  $cate2,
   $descriptions,
   $inventory,
-  $launch 
+  $launch,
 ]);
 
 // 如果stmt有新增欄位成功(rowcount=1,布林值為ture),output sucess 就呈現 true, echo 輸出結果
@@ -113,56 +93,3 @@ echo json_encode($output);
 
 
 $latest_sid = $pdo->lastInsertId(); //取得 PK
-
-
-# -------- 處理圖片
-$dir = __DIR__ . '/product-imgs/';
-
-# 檔案類型的篩選
-$exts = [
-  'image/jpeg' => '.jpg',
-  'image/png' => '.png',
-  'image/webp' => '.webp'
-];
-
-$output_img = [
-  'img-success' => false,
-  'file' => ''
-];
-
-
-if (!empty($_FILES) and !empty($_FILES['mainImg']) and $_FILES['mainImg']['error'] == 0) {
-
-  if (!empty($exts[$_FILES['mainImg']['type']])) {
-    $ext = $exts[$_FILES['mainImg']['type']]; // 副檔名
-
-    # 隨機的主檔名
-    $f = sha1($_FILES['mainImg']['name'] . uniqid());
-
-    # 將檔案直接存到資料夾(只要有送出資料挑一個必填欄位判斷(好像不能成功) 且 有拿到圖 就存起來到檔案夾)
-    if ( 
-      move_uploaded_file(
-        $_FILES['mainImg']['tmp_name'],
-        $dir . $f . $ext
-      )
-    ) {
-      $output_img['img-success'] = true;
-      $output_img['file'] = $f . $ext; //圖片名稱
-    }
-  }
-}
-
-$sql2 = "INSERT INTO `product_detail`(`sid`, `product_id`, `img`) VALUES ( ?, ?, ?)";
-
-$stmt2 = $pdo->prepare($sql2);
-
-$stmt2->execute([
-  $latest_sid,
-  $pid,
-  $dir . $f . $ext
-]);
-
-
-
-$output_img['img-success'] = boolval($stmt2->rowCount());
-echo json_encode($output_img); 
--- add-product.php
+++ add-product.php
@@ -3,6 +3,9 @@ require './index-parts/connect_db.php';
 $pageName = 'add';
 $title = '新增商品';
 
+$sql = "SELECT * FROM product_categories";
+$rows = $pdo->query($sql)->fetchAll();
+
 ?>
 <style>
   form .form-text {
@@ -22,7 +25,7 @@ $title = '新增商品';
           <form name="form1" onsubmit="sendData(event)">
             <div class="mb-3">
               <label for="product_id" class="form-label">商品編號</label>
-              <input type="text" class="form-control" id="product_id" name="product_id" disabled placeholder="Pxxxx (待確認新增商品後會自動生成)">
+              <input type="text" class="form-control" id="product_id" name="product_id"  placeholder="Pxxxx (待確認新增商品後會自動生成)">
               <div class="form-text"></div>
             </div>
             <div class="mb-3">
@@ -36,16 +39,22 @@ $title = '新增商品';
               <div class="form-text"></div>
             </div>
             <!-- 商品分類 下拉式選單 -->
-            <div class="mb-3">
-              <label for="category" class="form-label">商品分類</label>
-              <select name="category" id="category" class="form-control">
-                <option value="0">-- 請選擇類別 --</option>
+            <div class="input-group mb-3">
+              <span class="input-group-text">主分類</span>
+              <select class="form-select" name="cate1" id="cate1" onchange="generateCate2List()">
+                <?php foreach($rows as $r): 
+                  if($r['parent_sid'] == 0) :?>
+                <option value="<?= $r['sid'] ?>"><?= $r['name'] ?></option>
+                <?php endif;
+              endforeach; ?>
               </select>
-              <div class="form-text"></div>
-              <select name="subCategory" id="subCategory" class="form-control">
-              </select>
-              
             </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text">次分類</span>
+              <select class="form-select" name="cate2" id="cate2">
+              </select>
+            </div>
+
             <!-- 商品描述 -->
             <div class="mb-3">
               <label for="descriptions" class="form-label">商品描述</label>
@@ -67,14 +76,14 @@ $title = '新增商品';
               <label for="launch">下架</label>
             </div>
             <!-- 主要商品圖片 -->
-            <div class="mb-3">
+            <!-- <div class="mb-3">
               <label for="mainImg" class="form-label">主要商品圖片</label>
               <input type="file" class="form-control" name="mainImg" accept="image/jpeg,image/png,image/webp" onchange="previewImg(event)">
               <div class="form-text"></div>
               <div style="width:100px" class="showMainImg">
                 <img id="myimg" src="" alt="" width="100%" />
               </div>
-            </div>
+            </div> -->
             <!-- 新增商品／取消新增商品 按鈕 -->
             <div class="d-flex justify-content-center mb-3">
               <button type="submit" class="btn btn-warning rounded-pill">新增商品</button> &nbsp;
@@ -102,17 +111,38 @@ $title = '新增商品';
   const name_in = document.form1.name;
   const price_in = document.form1.price;
   const category = document.form1.category;
-  const selectedCategory = document.form1.category.value;
+  // const selectedCategory = document.form1.category.value;
   const descriptions = document.form1.descriptions;
-  const mainImg = document.form1.mainImg;
-  const moreImg = document.form1.moreImg;
+  // const mainImg = document.form1.mainImg;
+  // const moreImg = document.form1.moreImg;
   const inventory = document.form1.inventory;
   const launch = document.form1.launch.value;
-  const fields = [name_in, price_in, inventory, descriptions, category, mainImg];
+  const fields = [name_in, price_in, inventory, descriptions];
   const showMainImg = document.querySelector('.showMainImg');
 
-console.log(category);
-console.log(selectedCategory);
+
+  // 下拉選單的設定
+  const initVals = {cate1: 1, cate2: 5};
+  const cates = <?= json_encode($rows, JSON_UNESCAPED_UNICODE) ?>;
+  const cate1 = document.querySelector('#cate1');
+  const cate2 = document.querySelector('#cate2');
+
+  function generateCate2List() {
+    const cate1Val = cate1.value; // 主分類的值
+    let str = ""; // 要加進第二個選單的內容
+    // 跑迴圈看有哪些符合
+    for (let item of cates) {
+      if (+item.parent_sid === +cate1Val) { // 轉換成數值, cateVal 是字串
+        str += `<option value="${item.sid}">${item.name}</option>`;
+      }
+    }
+    cate2.innerHTML = str;
+  }
+
+  cate1.value = initVals.cate1; // 設定第一層的初始值
+  generateCate2List(); // 一進來就呼叫 / 生第二層
+  cate2.value = initVals.cate2; // 設定第二層的初始持
+
 
   // 預覽圖片 createObjectURL
   const previewImg = (event) => {
@@ -125,64 +155,6 @@ console.log(selectedCategory);
     }
   };
 
-  // 宣告商品類別 //改到資料庫一張類別表 categories
-  let product_category = [{
-      "category": "物品",
-      "subCategory": [{
-          v: "A",
-          name: "服裝"
-        },
-        {
-          v: "B",
-          name: "器材"
-        },
-        {
-          v: "C",
-          name: "裝備"
-        },
-      ]
-    },
-    {
-      "category": "食品",
-      "subCategory": [{
-          v: "D",
-          name: "蛋白"
-        },
-        {
-          v: "E",
-          name: "非蛋白"
-        },
-      ]
-    }
-  ];
-
-  // 下拉式選單 串接子選項
-  $.each(product_category, function(index, value) {
-    $("#category").append(`<option value="${index+1}">${value.category}</option>`)
-  })
-
-  // 先選到類別名，在第二個下拉式選單顯示該類別名的子選項
-  $("#category").on("change", function() {
-    let selectedCategory = $(this).val();
-    // console.log($(this).val());
-
-    $("#subCategory").empty();
-    // 商品類別是陣列 陣列的item是內容要對到他的類別名
-    // let selectedPc = product_category.find(function(item){
-    //     return item.category === selectedCategory;
-    // })
-    let selectedPc = product_category[selectedCategory - 1];
-
-    // 當選到類別
-    if (selectedPc) {
-      $.each(selectedPc.subCategory, function(index, value2) {
-        // console.log(value2);
-        $("#subCategory").append(`<option value="${value2.v}" >${value2.name}</option>`)
-        // console.log(selectedPc);
-      })
-    }
-  })
-
 
 
   // ---- 按下送出按鈕要執行以下
@@ -190,62 +162,62 @@ console.log(selectedCategory);
     e.preventDefault();
 
     // 外觀要回復原來的狀態
-    fields.forEach(field => {
-      field.style.border = '1px solid #CCCCCC';
-      if (field.nextElementSibling) {
-        field.nextElementSibling.innerHTML = '';
-      }
-    })
+    // fields.forEach(field => {
+    //   field.style.border = '1px solid #CCCCCC';
+    //   if (field.nextElementSibling) {
+    //     field.nextElementSibling.innerHTML = '';
+    //   }
+    // })
 
-    // 先假設表單都是正確資訊，後續判斷如果有誤就把它變成false
+    // // 先假設表單都是正確資訊，後續判斷如果有誤就把它變成false
     let isPass = true;
 
-    // 1.商品編號亂數或for給數字？ 在前端做？ 要怎麼知道資料庫已有的值
+    // // 1.商品編號亂數或for給數字？ 在前端做？ 要怎麼知道資料庫已有的值
 
 
-    // 2.判斷商品名稱需大於兩個字:如果長度小於二就是資訊有誤
-    if (name_in.value.length < 2) {
-      $isPass = false;
-      name_in.style.border = '2px solid red';
-      name_in.nextElementSibling.innerHTML = '請填寫正確的商品名稱';
-    }
-
-    //3.price 如果價格<1 就不是正確值
-    if (price_in.value <= 0) {
-      isPass = false;
-      price_in.style.border = '2px solid red';
-      price_in.nextElementSibling.innerHTML = '請填寫正確的商品價格';
-    }
-
-    // 4.category 如果value沒有值，就代表沒選 (尚未釐清) // 設定進去後還是會有
-    // if (selectedCategory === '0') {
-    //   isPass = false;
-    //   category.style.border = '2px solid red';
-    //   category.nextElementSibling.innerHTML = '請選擇商品類別';
+    // // 2.判斷商品名稱需大於兩個字:如果長度小於二就是資訊有誤
+    // if (name_in.value.length < 2) {
+    //   $isPass = false;
+    //   name_in.style.border = '2px solid red';
+    //   name_in.nextElementSibling.innerHTML = '請填寫正確的商品名稱';
     // }
 
-    // 5.判斷商品描述 需大於50字
-    if (descriptions.value.length < 10) {
-      $isPass = false;
-      descriptions.style.border = '2px solid red';
-      descriptions.nextElementSibling.innerHTML = '請填寫商品描述(需滿50字)';
-    }
+    // //3.price 如果價格<1 就不是正確值
+    // if (price_in.value <= 0) {
+    //   isPass = false;
+    //   price_in.style.border = '2px solid red';
+    //   price_in.nextElementSibling.innerHTML = '請填寫正確的商品價格';
+    // }
 
-    // 6.inventory 如果庫存沒有值 或 庫存<0代表資料有誤
-    if (!inventory.value || inventory.value < 0) {
-      isPass = false;
-      inventory.style.border = '2px solid red';
-      inventory.nextElementSibling.innerHTML = '請填寫庫存';
-    }
+    // // 4.category 如果value沒有值，就代表沒選 (尚未釐清) // 設定進去後還是會有
+    // // if (selectedCategory === '0') {
+    // //   isPass = false;
+    // //   category.style.border = '2px solid red';
+    // //   category.nextElementSibling.innerHTML = '請選擇商品類別';
+    // // }
+
+    // // 5.判斷商品描述 需大於50字
+    // if (descriptions.value.length < 10) {
+    //   $isPass = false;
+    //   descriptions.style.border = '2px solid red';
+    //   descriptions.nextElementSibling.innerHTML = '請填寫商品描述(需滿50字)';
+    // }
+
+    // // 6.inventory 如果庫存沒有值 或 庫存<0代表資料有誤
+    // if (!inventory.value || inventory.value < 0) {
+    //   isPass = false;
+    //   inventory.style.border = '2px solid red';
+    //   inventory.nextElementSibling.innerHTML = '請填寫庫存';
+    // }
 
     // 7.判斷上架狀態：預設為1,如果inventory填寫0就將launch設為0
 
     // 8.mainImg 如果圖片沒有值 代表資料有誤
-    if (!mainImg.value) {
-      isPass = false;
-      mainImg.style.border = '2px solid red';
-      mainImg.nextElementSibling.innerHTML = '請上傳商品圖片';
-    }
+    // if (!mainImg.value) {
+    //   isPass = false;
+    //   mainImg.style.border = '2px solid red';
+    //   mainImg.nextElementSibling.innerHTML = '請上傳商品圖片';
+    // }
 
     // 9.moreImg 非必填: 是否要判別跟主圖片一樣就提醒?
 
@@ -260,7 +232,7 @@ console.log(selectedCategory);
     const fd = new FormData(document.form1);
 
     // 只要有資料傳送時或是想暫存資料就可以用 AJAX 方式去叫小弟做事 fetch 這支 add-api.php
-    fetch('add-product-api.php', {
+    fetch("add-product-api.php", {
         method: 'POST',
         body: fd, // 送出資料格式會自動是mutipart/form-data
       }).then(r => r.json())
@@ -270,10 +242,10 @@ console.log(selectedCategory);
         });
       })
       .catch(ex => console.log(ex))
-      if (data.success) {
-        alert('資料新增成功');
-        location.href = "product_list.php";
-      }
+      // if (data.success) {
+      //   alert('資料新增成功');
+      //   location.href = "product_list.php";
+      // }
   }
 
 
