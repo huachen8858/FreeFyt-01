74bb4a95f24a927100b140cefeb8a9df5a284af2
huachen8858 <huachen8858@gmail.com>
2023-10-19 10:33:01 +0800

    upload img (可存檔案路徑到欄位 但sid有問題)

--- upload-img-api-1.php
+++ upload-img-api-1.php
@@ -1,44 +1,62 @@
 <?php
 require './index-parts/connect_db.php';
 
-// 如果有送出POST就執行下方將圖片存起來:好像不能成功
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $dir = __DIR__ . '/product-imgs/';
+$dir = __DIR__ . '/product-imgs/';
 
-  # 檔案類型的篩選
-  $exts = [
-    'image/jpeg' => '.jpg',
-    'image/png' => '.png',
-    'image/webp' => '.webp',
-  ];
+# 檔案類型的篩選
+$exts = [
+  'image/jpeg' => '.jpg',
+  'image/png' => '.png',
+  'image/webp' => '.webp',
+];
 
-  header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-  $output = [
-    'success' => false,
-    'file' => ''
-  ];
+$output = [
+  'success' => false,
+  'file' => ''
+];
 
-  if (!empty($_FILES) and !empty($_FILES['mainImg']) and $_FILES['mainImg']['error'] == 0) {
+$mainImg = $_POST['mainImg'] ?? '';
+// echo json_encode($output);
 
-    if (!empty($exts[$_FILES['mainImg']['type']])) {
-      $ext = $exts[$_FILES['mainImg']['type']]; // 副檔名
 
-      # 隨機的主檔名
-      $f = sha1($_FILES['mainImg']['name'] . uniqid());
+if (!empty($_FILES) and !empty($_FILES['mainImg']) and $_FILES['mainImg']['error'] == 0) {
 
-      # 將檔案直接存到資料夾(只要有送出資料挑一個必填欄位判斷(好像不能成功) 且 有拿到圖 就存起來到檔案夾)
-      if ( 
-        move_uploaded_file(
-          $_FILES['mainImg']['tmp_name'],
-          $dir . $f . $ext
-        )
-      ) {
-        $output['success'] = true;
-        $output['file'] = $f . $ext;
-      }
+  if (!empty($exts[$_FILES['mainImg']['type']])) {
+    $ext = $exts[$_FILES['mainImg']['type']]; // 副檔名
+
+    # 隨機的主檔名
+    $f = sha1($_FILES['mainImg']['name'] . uniqid());
+
+    # 將檔案直接存到資料夾
+    if (
+      move_uploaded_file(
+        $_FILES['mainImg']['tmp_name'],
+        $dir . $f . $ext
+      )
+    ) {
+      $output['success'] = true;
+      $output['file'] = $f . $ext;
     }
   }
+}
 
-  echo json_encode($output);
-// }
+$latest_sid = $pdo->lastInsertId(); //取得 PK
+$imgurl = $dir . $f . $ext;
+
+$sql = "INSERT INTO `product_detail`(`product_sid`, `img`) VALUES (?, ?)";
+
+
+// pdo 先準備：並沒有真的執行，會先拿到pdo statement 的物件
+$stmt = $pdo->prepare($sql);
+
+// pdo stmt 執行：把表單拿到的值丟到上方的 ?
+$stmt->execute([
+  $latest_sid,
+  $imgurl
+]);
+
+// 如果stmt有新增欄位成功(rowcount=1,布林值為ture),output sucess 就呈現 true, echo 輸出結果
+$output['success'] = boolval($stmt->rowCount());
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
