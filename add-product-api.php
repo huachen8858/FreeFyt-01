024f7f90d76e8b053b6862936c32375d4ed88b78
huachen8858 <huachen8858@gmail.com>
2023-10-19 20:57:51 +0800

    上傳資料api合併圖片上傳功能

--- add-product-api.php
+++ add-product-api.php
@@ -14,8 +14,8 @@ $output = [
 // exit;
 
 
-// 資料寫入前要檢查: 除更多圖片其他欄位必填
-if (empty($_POST['name']) or empty($_POST['price']) or empty($_POST['cate1']) or empty($_POST['cate2']) or empty($_POST['descriptions']) or empty($_POST['inventory']) or empty($_POST['launch'])) {
+// 資料寫入前要檢查: 
+if (empty($_POST['name']) or empty($_POST['price']) or empty($_POST['cate1']) or empty($_POST['cate2']) or empty($_POST['descriptions'])) {
   $output['errors']['form'] = "缺少欄位資料";
   echo json_encode($output, JSON_UNESCAPED_UNICODE);
   exit;
@@ -110,3 +110,60 @@ echo json_encode($output, JSON_UNESCAPED_UNICODE);
 
 
 // $latest_sid = $pdo->lastInsertId(); //取得 PK
+
+
+# 上傳圖片檔名到 product_detail ------------------------------
+$dir = __DIR__ . '/product-imgs/';
+
+# 檔案類型的篩選
+$exts = [
+  'image/jpeg' => '.jpg',
+  'image/png' => '.png',
+  'image/webp' => '.webp',
+];
+
+
+$output_img = [
+  'success' => false,
+  'file' => ''
+];
+
+$mainImg = $_POST['mainImg'] ?? '';
+// echo json_encode($output);
+$f = ''; // 初始化 $f 為空字串
+$ext = '';
+
+if (!empty($_FILES) and !empty($_FILES['mainImg']) and $_FILES['mainImg']['error'] == 0) {
+
+  if (!empty($exts[$_FILES['mainImg']['type']])) {
+    $ext = $exts[$_FILES['mainImg']['type']]; // 副檔名
+
+    # 隨機的主檔名
+    $f = sha1($_FILES['mainImg']['name'] . uniqid());
+
+    # 將檔案直接存到資料夾
+    if (
+      move_uploaded_file(
+        $_FILES['mainImg']['tmp_name'],
+        $dir . $f . $ext
+      )
+    ) {
+      $output_img['success'] = true;
+      $output_img['file'] = $f . $ext;
+    }
+    $imgurl = $f . $ext; // 只存檔名就好
+
+    $sql_img = "INSERT INTO `product_detail`(`product_sid`, `img`) VALUES (?, ?)";
+
+
+    $stmt_img = $pdo->prepare($sql_img);
+
+    $stmt_img->execute([
+      $pid,
+      $imgurl
+    ]);
+  }
+}
+
+$output_img['success'] = boolval($stmt_img->rowCount());
+echo json_encode($output_img, JSON_UNESCAPED_UNICODE);
