2c721dec1c5b79027eae843a7a4a599662e5c858
huachen8858 <huachen8858@gmail.com>
2023-10-20 10:32:53 +0800

    confirm comment

--- product_list.php
+++ product_list.php
@@ -31,8 +31,8 @@ if ($totalRows > 0) {
 
     $sql = sprintf(
         "SELECT * FROM product_list ORDER BY sid DESC LIMIT %s, %s",
-        ($page - 1) * $perPage, 
-        $perPage 
+        ($page - 1) * $perPage,
+        $perPage
     );
     $rows = $pdo->query($sql)->fetchAll();
 }
@@ -50,86 +50,91 @@ if ($totalRows > 0) {
             <h5 class="m-0 font-weight-bold text-primary">總商品列表</h5>
             <div class="btn btn-primary rounded-pill">
                 <a class="text-light" href="add-product.php"><i class="fas fa-plus"></i> 新增商品</a>
+            </div>
         </div>
-    </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>商品編號</th>
-                        <th>商品名稱</th>
-                        <th>商品價格</th>
-                        <th>商品描述</th>
-                        <th>庫存量</th>
-                        <th>累積購買數</th>
-                        <th>建立日期</th>
-                        <th>是否上架</th>
-                        <th><i class="far fa-trash-alt"></i></th>
-                        <th><i class="far fa-edit"></i></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($rows as $r) :?>
-                    <tr>
-                        <td><?= $r['sid'] ?></td>
-                        <td><?= $r['product_id'] ?></td>
-                        <td><?= htmlentities($r['name']) ?></td>
-                        <td><?= $r['price'] ?></td>
-                        <!-- 隱藏多於文字text-truncate??? -->
-                        <td class=""><?= htmlentities($r['descriptions']) ?></td>
-                        
-                        <td><?= $r['inventory'] ?></td>
-                        <td><?= $r['purchase_qty'] ?></td>
-                        <td><?= $r['create_date'] ?></td>
-                        <?php if(!$r['launch']) : ?>
-                        <td><div class="btn btn-secondary rounded-pill">未上架</div></td>
-                        <?php else : ?>
-                        <td><div class="btn btn-success rounded-pill">上架中</div></td>
-                        <?php endif; ?>
-                        <td><a href="javascript: deleteItem(<?= $r['sid'] ?>)"><i class="far fa-trash-alt"></a></td>
-                        <td><a href="edit-product.php?sid=<?= $r['sid'] ?>"><i class="far fa-edit"></a></td>
-                    </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <!-- 總列數/總頁數 -->
-    <div><?= "$totalRows / $totalPages" ?></div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>商品編號</th>
+                            <th>商品名稱</th>
+                            <th>商品價格</th>
+                            <th>商品描述</th>
+                            <th>庫存量</th>
+                            <th>累積購買數</th>
+                            <th>建立日期</th>
+                            <th>是否上架</th>
+                            <th><i class="far fa-trash-alt"></i></th>
+                            <th><i class="far fa-edit"></i></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($rows as $r) : ?>
+                            <tr>
+                                <td><?= $r['sid'] ?></td>
+                                <td><?= $r['product_id'] ?></td>
+                                <td><?= htmlentities($r['name']) ?></td>
+                                <td><?= $r['price'] ?></td>
+                                <!-- 隱藏多於文字text-truncate??? -->
+                                <td class=""><?= htmlentities($r['descriptions']) ?></td>
 
-    <!-- pagination:還沒調整完 -->
-    <div class="raw">
-        <div class="col d-flex justify-content-end">
-            <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>">
-                        <a class="page-link" href="?page=1">
-                        <i class="fa-solid fa-angles-left"></i></a>
-                    </li>
-                    <?php for ($i = $page - 3; $i <= $page + 3; $i++) :
-                        if ($i >= 1 and $i <= $totalPages) : ?>
-                    <li class="page-item <?= $i == $page ? 'active' : '' ?>">
-                        <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                                <td><?= $r['inventory'] ?></td>
+                                <td><?= $r['purchase_qty'] ?></td>
+                                <td><?= $r['create_date'] ?></td>
+                                <?php if (!$r['launch']) : ?>
+                                    <td>
+                                        <div class="btn btn-secondary rounded-pill">未上架</div>
+                                    </td>
+                                <?php else : ?>
+                                    <td>
+                                        <div class="btn btn-success rounded-pill">上架中</div>
+                                    </td>
+                                <?php endif; ?>
+                                <td><a href="javascript: deleteItem(<?= $r['sid'] ?>)"><i class="far fa-trash-alt"></a></td>
+                                <td><a href="edit-product.php?sid=<?= $r['sid'] ?>"><i class="far fa-edit"></a></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <!-- 總列數/總頁數 -->
+        <div><?= "$totalRows / $totalPages" ?></div>
+
+        <!-- pagination:還沒調整完 -->
+        <div class="raw">
+            <div class="col d-flex justify-content-end">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>">
+                            <a class="page-link" href="?page=1">
+                                <i class="fa-solid fa-angles-left"></i></a>
                         </li>
-                        <?php endif; ?>
-                    <?php endfor; ?>
-                    <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>">
-                        <a class="page-link" href="?page=<?= $totalPages ?>">
-                        <i class="fa-solid fa-angles-right"></i></a>
-                    </li>
-                </ul>
-            </nav>
+                        <?php for ($i = $page - 3; $i <= $page + 3; $i++) :
+                            if ($i >= 1 and $i <= $totalPages) : ?>
+                                <li class="page-item <?= $i == $page ? 'active' : '' ?>">
+                                    <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                                </li>
+                            <?php endif; ?>
+                        <?php endfor; ?>
+                        <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>">
+                            <a class="page-link" href="?page=<?= $totalPages ?>">
+                                <i class="fa-solid fa-angles-right"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     </div>
-</div>
 
 </div>
 <?php include './index-parts/footerToScripts.php' ?>
 <script>
     function deleteItem(sid) {
-        if (confirm(`確定要刪除編號 ${sid} 的資料嗎?`)) {
+        if (confirm(`確定要刪除編號 ${sid} 的資料嗎?
+提醒您若確定刪除資料將無法復原，可透過下架商品保留商品資訊。`)) {
             location.href = 'delete-product.php?sid=' + sid;
         }
     }
